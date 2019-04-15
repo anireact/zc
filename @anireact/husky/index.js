7d364e96c154e69654b0f21990a75c2b1ea32027
miyaokamarina <miyaokamarina@gmail.com>
2019-04-15 21:49:29 +0300

    fix(husky): Fixed Commitlint config path.

--- @anireact/husky/index.js
+++ @anireact/husky/index.js
@@ -3,6 +3,6 @@ const join = require('command-join');
 module.exports = {
     hooks: {
         'pre-push': 'zc build && zc lint',
-        'commit-msg': `commitlint --config ${join(require.resolve('../implicit/commitlint'))} -E HUSKY_GIT_PARAMS`,
+        'commit-msg': `commitlint --config ${join(require.resolve('./commitlint'))} -E HUSKY_GIT_PARAMS`,
     },
 };
