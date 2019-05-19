55f0940fec55dfaa4f5d5abd85f33f9e0d587991
miyaokamarina <miyaokamarina@gmail.com>
2019-05-20 01:50:11 +0300

    feat: Dependencies upgraded.

--- @anireact/example-rollup/src/index.ts
+++ @anireact/example-rollup/src/index.ts
@@ -2,6 +2,10 @@ import { f } from '@anireact/example-dependency';
 
 import { i } from './i';
 
-export const g = (a: number) => f(a, 1);
+export const g = async (a: number) => (await Promise.resolve(f(a, 1))) + 1;
 export const h = (a: readonly number[]) => a;
 export const j = i(0); // tslint:disable-line no-unsafe-any
+
+export * from './k';
+
+export * from 'upath';
---
+++ @anireact/example-rollup/src/k.ts
@@ -0,0 +1,3 @@
+export interface K {
+    readonly l: string;
+}
